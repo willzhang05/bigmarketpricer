5b519c078cd4f1bbc5fc4f20a3c6f11c738e6c77
Dylan <dylanjones2011@gmail.com>
2017-03-26 08:52:00 -0400

    broke node scritp

--- wrangle.js
+++ wrangle.js
@@ -1,12 +1,14 @@
 const searchTerm = process.argv[2];
+const args = process.argv;
 const argv = require('minimist')(process.argv.slice(3));
 console.log('Searching for: ' + searchTerm);
 
 const options = {
     baseHost: ('baseHost' in argv ? argv['baseHost'] : 'craigslist.org'),  // default org
-    category: ('category' in argv ? argv['category'] : ''),                // temporary syp
+    category: (args.length >= 5 ? args[4] : ''),
+	//    category: ('category' in argv ? argv['category'] : ''),                // temporary syp
     city: ('city' in argv ? argv['city'] : 'washingtondc'),                // default dc
-    maxAsk: ('maxAsk' in argv ? Math.ceil(argv['maxAsk']).toString() : ''),
+    maxAsk: (args.length >= 4 ? Math.ceil(args[3]).toString() : ''),
     minAsk: ('minAsk' in argv ? Math.ceil(argv['maxAsk']).toString() : ''),
 };
 
